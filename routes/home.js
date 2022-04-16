86264170545f59592a5ac5cb4ee52e544e9cf070
Edgar <edgarnikogosian89@gmail.com>
2022-04-16 12:55:13 +0400

    privacy

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const dailyArticlesController = require("../controllers/dailyArticles");
 
+router.get("/privacy", (req, res) => {
+  res.render("privacy", { currentTab: "" });
+});
 router.get("/", dailyArticlesController.dailyArticle);
 
 module.exports = router;
